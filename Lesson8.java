ad08bca528bf77b5f5caafe213f6fe33aae3c0d2
Merve Erkan <117810757+merveErkan@users.noreply.github.com>
2024-02-29 19:48:56 +0300

    Create Lesson8.java

---
+++ Lesson8.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Lesson8 {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5, 6, 1, 2, 1, 1, 1, 1, 1, 1, 5, 6};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+            else
+                map.put(arr[i], 1);
+        }
+        System.out.println(map);
+    }
+}
